4b47b56a4d506ea1802e25bfd34f0a86daa85845
paulooctavioINTELI <124174379+paulooctavioINTELI@users.noreply.github.com>
2023-11-15 16:10:21 -0300

    ajust result text

--- src/app/pages/auctionResult/auctionResult.tsx
+++ src/app/pages/auctionResult/auctionResult.tsx
@@ -9,17 +9,21 @@ interface WinnerProps {
 
 export default function AuctionResult({playerWinner, winningPrice, goTo}: WinnerProps) {
     return (
-        <div className="flex flex-grow w-full items-center justify-center px-1">
-            <h1 className="text-3xl font-bold text-center">
-                O arrematante <span className="text-[#D19E44]">{playerWinner}</span> arrematou por <span className="text-[#D19E44]">$ {winningPrice}</span>
-            </h1>
+        <div className="flex flex-grow flex-col w-full items-center justify-center px-16 gap-4">
+            <div className="flex flex-col items-center justify-center">
+                <h1 className="text-4xl w-full font-bold text-center">
+                    O arrematante <span className="text-[#D19E44]">{playerWinner}</span> arrematou por
+                </h1>
+                <span className="text-[#D19E44] text-4xl w-full font-bold text-center">$ {winningPrice}</span>
+
+            </div>
 
             <Button
                 color="bg-[#ffca6e]"
                 textSize=""
                 onClick={goTo}
                 altCss="w-full"
-            >Continuar</Button>
+            >Continusar</Button>
         </div>
     )
 }
